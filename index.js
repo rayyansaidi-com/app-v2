4064649603ffcca0696176ac7861b8383b384de9
MrSun10 <63111246+MrSun10@users.noreply.github.com>
2020-07-09 17:20:59 -0400

    autoupdate

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const { app, BrowserWindow, Menu } = require('electron');
+const { app, BrowserWindow, Menu, autoUpdater } = require('electron');
 const shell = require('electron').shell;
 const electron = require('electron');
 const path = require('path');
+const server = 'https://hazel.mrsun10.vercel.app/'
+const url = `${server}/update/${process.platform}/${app.getVersion()}`
 
 function createWindow() {
   // Create the browser window.
@@ -37,6 +39,24 @@ function createWindow() {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(createWindow)
+//Autoupdate
+autoUpdater.setFeedURL({ url })
+setInterval(() => {
+  autoUpdater.checkForUpdates()
+}, 60000)
+autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+  const dialogOpts = {
+    type: 'info',
+    buttons: ['Restart', 'Later'],
+    title: 'Application Update',
+    message: process.platform === 'win32' ? releaseNotes : releaseName,
+    detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+  }
+
+  dialog.showMessageBox(dialogOpts).then((returnValue) => {
+    if (returnValue.response === 0) autoUpdater.quitAndInstall()
+  })
+})
 
 /* Quit when all windows are closed, except on macOS. There, it's common
  for applications and their menu bar to stay active until the user quits
