f28ed1d99d0269e6a978e6bf62bfd8535a13ea95
Nathan Chu <63111210+nthnchu@users.noreply.github.com>
2020-07-11 20:20:10 -0400

    index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const { app, BrowserWindow, Menu, autoUpdater } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
 const shell = require('electron').shell;
 const electron = require('electron');
 const path = require('path');
-const server = 'https://hazel.mrsun10.vercel.app/'
-const url = `${server}/update/${process.platform}/${app.getVersion()}`
+// const server = 'https://hazel.mrsun10.vercel.app/'
+// const url = `${server}/update/${process.platform}/${app.getVersion()}`
 
 function createWindow() {
   // Create the browser window.
@@ -25,6 +25,12 @@ function createWindow() {
           click() {
             app.quit()
           }
+        },
+        {
+          label: 'Open DevTools',
+          click() {
+            win.webContents.openDevTools()
+          }
         }
       ]
     }
@@ -40,23 +46,23 @@ function createWindow() {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(createWindow)
 //Autoupdate
-autoUpdater.setFeedURL({ url })
-setInterval(() => {
-  autoUpdater.checkForUpdates()
-}, 60000)
-autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
-  const dialogOpts = {
-    type: 'info',
-    buttons: ['Restart', 'Later'],
-    title: 'Application Update',
-    message: process.platform === 'win32' ? releaseNotes : releaseName,
-    detail: 'A new version has been downloaded. Restart the application to apply the updates.'
-  }
+// autoUpdater.setFeedURL({ url })
+// setInterval(() => {
+//   autoUpdater.checkForUpdates()
+// }, 60000)
+// autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+//   const dialogOpts = {
+//     type: 'info',
+//     buttons: ['Restart', 'Later'],
+//     title: 'Application Update',
+//     message: process.platform === 'win32' ? releaseNotes : releaseName,
+//     detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+//   }
 
-  dialog.showMessageBox(dialogOpts).then((returnValue) => {
-    if (returnValue.response === 0) autoUpdater.quitAndInstall()
-  })
-})
+//   dialog.showMessageBox(dialogOpts).then((returnValue) => {
+//     if (returnValue.response === 0) autoUpdater.quitAndInstall()
+//   })
+// })
 
 /* Quit when all windows are closed, except on macOS. There, it's common
  for applications and their menu bar to stay active until the user quits
