a0066be55bd40794a618c0a7a440485025d78f82
MrSun10 <63111246+MrSun10@users.noreply.github.com>
2020-07-25 19:00:58 -0400

    hi

--- src/renderer.js
+++ src/renderer.js
@@ -1,20 +1,36 @@
 const semverGt = require('semver/functions/gt')
 const appVersion = require("electron").remote.app.getVersion();
+const { dialog, shell } = require("electron").remote
 var request = new XMLHttpRequest();
 request.open('GET', 'https://api.github.com/repositories/275879561/releases', true);
-
+const isMacOS = process.platform
+const isWin32 = process.platform
+const isLinux = process.platform
+if (process.platform == "darwin")
 request.onload = function() {
   if (this.status >= 200 && this.status < 400) {
     // Success!
     var data = JSON.parse(this.response);
     data = data[0]["tag_name"]
-    if (semverGt(data, appVersion)) {
-    console.log("update!")} else {
-      console.log("don't")
+    if (semverGt("2.2.0", appVersion)) {
+    console.log("update!");
+    const updateDialog = dialog.showMessageBoxSync({
+      "buttons": [
+        "Update for your OS", "I don't want to update"
+      ],
+      "defaultId": 0,
+      "message": "There's an update for this app."
+    }
+    );
+    if (updateDialog == 0) {
+      shell.openExternal('https://github.com/rayyansaidi-com/app/releases/latest')
+    }
+    } else {
+      console.log("You have the newest version")
     }
   } else {
     // We reached our target server, but it returned an error
-    console.error("Error! Autoupdate is broken")
+    console.error("Error! Autoupdate is broken. Report an error on github at https://github.com/rayyansaidi-com/app/issues/new")
   }
 };
 
