169d91958ce75284a9798a853a2903fcd4f17b04
MrSun10 <63111246+MrSun10@users.noreply.github.com>
2020-07-26 15:11:28 -0400

    hello

--- src/renderer.js
+++ src/renderer.js
@@ -3,39 +3,78 @@ const appVersion = require("electron").remote.app.getVersion();
 const { dialog, shell } = require("electron").remote
 var request = new XMLHttpRequest();
 request.open('GET', 'https://api.github.com/repositories/275879561/releases', true);
-const isMacOS = process.platform
-const isWin32 = process.platform
-const isLinux = process.platform
-if (process.platform == "darwin")
-request.onload = function() {
-  if (this.status >= 200 && this.status < 400) {
-    // Success!
-    var data = JSON.parse(this.response);
-    data = data[0]["tag_name"]
-    if (semverGt("2.2.0", appVersion)) {
-    console.log("update!");
-    const updateDialog = dialog.showMessageBoxSync({
-      "buttons": [
-        "Update for your OS", "I don't want to update"
-      ],
-      "defaultId": 0,
-      "message": "There's an update for this app."
-    }
-    );
-    if (updateDialog == 0) {
-      shell.openExternal('https://github.com/rayyansaidi-com/app/releases/latest')
-    }
-    } else {
-      console.log("You have the newest version")
-    }
-  } else {
-    // We reached our target server, but it returned an error
-    console.error("Error! Autoupdate is broken. Report an error on github at https://github.com/rayyansaidi-com/app/issues/new")
-  }
+const os = process.platform
+request.onload = function () {
+	if (this.status >= 200 && this.status < 400) {
+		// Success!
+		var data = JSON.parse(this.response);
+		data = data[0]["tag_name"]
+		if (semverGt(data, appVersion)) {
+			console.log("update!");
+			if (os == "darwin") {
+				const updateDialog = dialog.showMessageBoxSync({
+					"buttons": [
+						"Update for macOS", "I don't want to update"
+					],
+					"defaultId": 0,
+					"message": "There's an update for this app."
+				}
+				);
+				if (updateDialog == 0) {
+					shell.openExternal('https://github.com/rayyansaidi-com/app/releases/latest/download/rayyansaidi-desktop.dmg')
+				}
+			};
+			if (os == "linux") {
+				const updateDialog = dialog.showMessageBoxSync({
+					"buttons": [
+						"Update for macOS", "I don't want to update"
+					],
+					"defaultId": 0,
+					"message": "There's an update for this app."
+				}
+				);
+				if (updateDialog == 0) {
+					shell.openExternal('https://github.com/rayyansaidi-com/app/releases/latest/download/rayyansaidi-desktop.dmg')
+				}
+			};
+			if (os == "linux") {
+				const updateDialog = dialog.showMessageBoxSync({
+					"buttons": [
+						"Update for Linux", "I don't want to update"
+					],
+					"defaultId": 0,
+					"message": "There's an update for this app."
+				}
+				);
+				if (updateDialog == 0) {
+					shell.openExternal('https://github.com/rayyansaidi-com/app/releases/latest/download/rayyansaidi-desktop.snap')
+				}
+      };
+      if (os == "win32") {
+				const updateDialog = dialog.showMessageBoxSync({
+					"buttons": [
+						"Update for Windows", "I don't want to update"
+					],
+					"defaultId": 0,
+					"message": "There's an update for this app."
+				}
+				);
+				if (updateDialog == 0) {
+					shell.openExternal('https://github.com/rayyansaidi-com/app/releases/latest/download/rayyansaidi-desktop.exe')
+				}
+			};
+    } 
+    else {
+			console.log("You have the newest version")
+		}
+	} else {
+		// We reached our target server, but it returned an error
+		console.error("Error! Autoupdate is broken. Report an error on github at https://github.com/rayyansaidi-com/app/issues/new")
+	}
 };
 
-request.onerror = function() {
-  // There was a connection error of some sort
+request.onerror = function () {
+	// There was a connection error of some sort
 };
 
 request.send();
